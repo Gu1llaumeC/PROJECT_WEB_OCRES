c4b734493d9b7565828200eb9506f657d75ca2e5
Gui1llaumeC <guillaume.chollet@edu.ece.fr>
2021-12-07 08:43:45 +0100

    backend_V1

--- backend/functions/controller.js
+++ backend/functions/controller.js
@@ -1,3 +1,4 @@
+const { ObjectID } = require('bson');
 const QuoteModel = require('../models/quote.model');
 
 module.exports.add = async (req, res) => {
@@ -10,5 +11,31 @@ module.exports.add = async (req, res) => {
     }
     catch(err){
         res.status(200).send({err});
+    }
+}
+
+module.exports.getRandom = async(req, res) => {
+    const quotes = await QuoteModel.find().select();
+    var rand_i = Math.random() * (quotes.length);
+    rand_i = Math.floor(rand_i);
+    res.status(200).json(quotes[rand_i]);
+}
+
+module.exports.delete = async(req,res) => {
+    if (!ObjectID.isValid(req.params.id))
+    return res.status(400).send('ID unknown : ' + req.params.id)
+
+    try
+    {
+        await QuoteModel.deleteOne({_id : req.params.id}).exec();
+        res.status(200).json({
+            message : "Successfully deleted!"
+        });
+    }
+    catch(err)
+    {
+        return res.status(500).json({
+            message : err
+        });
     }
 }
--- backend/routes/quote.js
+++ backend/routes/quote.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
-const controller = require('../functions/controller')
+const controller = require('../functions/controller');
 
 router.post('/add', controller.add);
+
+router.get('/random',controller.getRandom);
+
+router.delete('/delete/:id',controller.delete);
 
 module.exports = router;
